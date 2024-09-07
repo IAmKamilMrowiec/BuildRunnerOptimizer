722a77e3f2175ba24d2b13ecd9d0fdd189b354b3
Kamil Mrowiec <i.am.kamil.mrowiec@gmail.com>
2024-09-07 19:05:57 +0200

    Add init project

---
+++ bin/build_runner_optimizer.dart
@@ -0,0 +1,5 @@
+import 'package:build_runner_optimizer/build_runner_optimizer.dart' as build_runner_optimizer;
+
+void main(List<String> arguments) {
+  print('Hello world: ${build_runner_optimizer.calculate()}!');
+}
---
+++ test/build_runner_optimizer_test.dart
@@ -0,0 +1,8 @@
+import 'package:build_runner_optimizer/build_runner_optimizer.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
